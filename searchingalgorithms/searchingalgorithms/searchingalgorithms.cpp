afb1f7c951f28872d3ab7c8c821c76fe0adc441d
AhbedShawarma <45717469+AhbedShawarma@users.noreply.github.com>
2019-04-16 00:10:26 -0400

    started searching algo

---
+++ searchingalgorithms/searchingalgorithms/searchingalgorithms.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+
+int main()
+{
+	std::ifstream mazeFile("maze.txt");
+	std::string input;
+	int height;
+	int width;
+	std::vector<std::string> maze;
+	//std::string maze;
+	std::getline(mazeFile, input);
+	height = std::stoi(input);
+	std::getline(mazeFile, input);
+	width = std::stoi(input);
+
+	for (int i = 0; i < height; i++) {
+		/*for (int j = 0; j < width; j++) {
+			mazeFile >> input;
+			maze += input;
+			
+		}*/
+		//mazeFile >> input;
+		//maze += input;
+		std::getline(mazeFile, input);
+		maze.push_back(input);
+	}
+	//std::cout << maze/*[0 + (1 * width)]*/;
+
+	for (int i = 0; i < height; i++) {
+		/*for (int j = 0; j < width; j++) {
+			int num = j + (i * width);
+			std::cout << maze[num];
+		}
+		std::cout << std::endl;*/
+		std::cout << maze[i] << std::endl;
+	}
+
+}
